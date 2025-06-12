61ba7dfcbe13e247f41008b1cee216606d146f08
PRASADD <durgaaprasadd765@gmail.com>
2025-06-12 21:13:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,20 @@
 resource "aws_instance" "app_instance" {
-  ami           = "ami-0a89b8a7f72b9cb5d" # Example AMI ID
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = module.vpc.public_subnets[0]
-  depends_on    = [
+  ami                         = "ami-0a89b8a7f72b9cb5d" # Use your region-specific AMI
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = templatefile("${path.module}/automate.sh.tmpl", {
+    config_file = file("${path.module}/configs/dev_config.json")
+  })
+
+  depends_on = [
     module.vpc,
     aws_security_group.web_sg
   ]
-  user_data = templatefile("${path.module}/automate.sh.tmpl", {
-    config_file = "configs/${local.config_file}"
-  })
 
   tags = {
     Name = "${var.stage}-${var.instance_name}"
