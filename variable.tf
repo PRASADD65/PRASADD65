b7f2dd9cbf6d2ab0f2d698099f1212fba9c64de6
PRASADD <durgaaprasadd765@gmail.com>
2025-06-13 17:06:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,54 +1,62 @@
 variable "stage" {
   description = "Deployment stage (dev or prod)"
   type        = string
+  default     = "dev"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "The EC2 instance type."
   type        = string
   default     = "t3.micro"
 }
 
+# Added instance_name variable
 variable "instance_name" {
-  description = "Name of the EC2 instance"
+  description = "Name of the EC2 instance (used in tags)."
   type        = string
+  default     = "TechEazy"
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC"
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC."
   type        = string
-  default        = "VPC"
+  default     = "10.0.0.0/16"
 }
 
-variable "az_name" {
-  description = "Name of the az"
-  type        = string
+variable "public_subnet_cidrs" {
+  description = "A list of CIDR blocks for the public subnets."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "github_repo_url" {
-  description = "URL of the GitHub repository"
+variable "private_subnet_cidrs" {
+  description = "A list of CIDR blocks for the private subnets."
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+# Github_repo_url
+variable "repo_url" {
+  description = "The URL of the Git repository containing the application code."
   type        = string
-  default     = "https://github.com/techeazy-consulting/techeazy-devops.git" # Add your GitHub URL here
+  default     = "https://github.com/techeazy-consulting/techeazy-devops.git"
 }
 
 variable "key_name" {
-  description = "Name of the AWS Key Pair to SSH into the instance"
+  description = "The name of the AWS EC2 Key Pair to use for SSH access."
   type        = string
 }
 
 variable "s3_bucket_name" {
   description = "The globally unique name for the S3 bucket where logs will be stored."
   type        = string
-  # No default to enforce explicit naming, fulfilling the requirement
-  # "name should be configurable; if not provided, terminate with error"
+  # No default here, as it must be explicitly provided and globally unique
 }
 
-# New variable for instance shutdown time (full cron expression)
 variable "shutdown_time" {
   description = "The cron expression (5 parts: Minute Hour DayOfMonth Month DayOfWeek) for when the EC2 instance should shut down and logs are backed up. E.g., '40 18 * * *' for 6:40 PM daily."
   type        = string
   validation {
-    # Simple validation: ensure it's a non-empty string with at least 4 spaces (5 fields)
     condition     = length(regexall("\\s", var.shutdown_time)) == 4 && length(var.shutdown_time) > 0
     error_message = "Shutdown time must be a 5-part cron expression (e.g., '0 17 * * *')."
   }
